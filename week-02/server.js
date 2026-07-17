dd0d79902d3b501b0079feaf3cedae11814e33a2
mukim shah <mukimshah6568@gmail.com>
2026-07-17 20:36:42 +0530

    Implement task CRUD operations

--- week-02/server.js
+++ week-02/server.js
@@ -4,6 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 
 // Root Endpoint
 app.get("/", (req, res) => {
@@ -81,6 +82,74 @@ app.get("/tasks/:id", (req, res) => {
     res.status(200).json(task);
 });
 
+
+// POST Endpoint
+app.post("/tasks", (req, res) => {
+
+    
+    const newTask = {
+        id: tasks.length + 1,
+        title: req.body.title,
+        done: req.body.done
+    };
+
+    tasks.push(newTask);
+
+    res.status(201).json({
+        success: true,
+        message: "Task created successfully.",
+        task: newTask
+    });
+});
+
+// PUT Endpoint
+app.put("/tasks/:id", (req, res) => {
+
+    const task = tasks.find((task) => {
+        return task.id === Number(req.params.id);
+    });
+
+    if (!task) {
+        return res.status(404).json({
+            success: false,
+            message: "Task not found"
+        });
+    }
+
+   
+
+    task.title = req.body.title;
+    task.done = req.body.done;
+
+    res.status(200).json({
+        success: true,
+        message: "Task updated successfully.",
+        task: task
+    });
+});
+
+// DELETE Endpoint
+app.delete("/tasks/:id", (req, res) => {
+
+    const taskIndex = tasks.findIndex((task) => {
+        return task.id === Number(req.params.id);
+    });
+
+    if (taskIndex === -1) {
+        return res.status(404).json({
+            success: false,
+            message: "Task not found"
+        });
+    }
+
+    const deletedTask = tasks.splice(taskIndex, 1);
+
+    res.status(200).json({
+        success: true,
+        message: "Task deleted successfully.",
+        task: deletedTask[0]
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
